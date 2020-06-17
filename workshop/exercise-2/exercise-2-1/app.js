0876a078013f6bf27ed6db0d650f22295b1b1a2b
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-06-17 15:09:54 -0400

    Exercice-2.1

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -2,9 +2,18 @@
 const FROGS = 3;
 
 // 1. Create for loop that makes use of FROGS to know how many lanes to create.
-
 // 2. Create li
-
 // 3. Create span and add it to the li
-
 // 4. Assign an id to each lane
+
+const track = document.querySelector('#track');
+
+for (let i = 0; i < FROGS; i++) {
+    let lane = document.createElement('LI');
+    let span = document.createElement('SPAN');
+    
+    lane.setAttribute('id', `lane-${i + 1}`);
+    span.innerText = `Lane ${i + 1}`;
+    
+    track.appendChild(lane).appendChild(span);
+}
