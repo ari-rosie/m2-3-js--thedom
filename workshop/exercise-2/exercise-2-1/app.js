f7f22113f2ba6b4ffdccc329ac457772d8d64201
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-06-17 21:18:01 -0400

    Exercice-2.3

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,5 +1,8 @@
 // Preset values
 const FROGS = 3;
+
+
+//creating the track field
 const track = document.querySelector('#track');
 
 for (let i = 0; i < FROGS; i++) {
@@ -7,11 +10,13 @@ for (let i = 0; i < FROGS; i++) {
     let span = document.createElement('SPAN');
     
     lane.setAttribute('id', `lane-${i + 1}`);
-    span.innerText = `Lane ${i + 1}`;
+    span.innerText = `${i + 1}`;
     
     track.appendChild(lane).appendChild(span);
 }
 
+
+//creating the racers array
 let racers = [];
 let randomN;
 
@@ -20,4 +25,25 @@ for (let i = 0; i < FROGS; i++) {
     racers.push(frogStable[randomN]);
     frogStable.splice(randomN, 1);
 }
+
+
+//adding each racer to its lane
+racers.forEach(function(racer) {
+    racer.progress = 0;
+
+    let div = document.createElement('DIV');
+    div.classList.add('frog');
+    div.innerText = `${racer.name} ${racer.number} ${racer.progress}`;
+
+    let position = racers.indexOf(racer);
+    let frogLane = document.querySelector(`#lane-${position + 1}`);
+   
+    frogLane.appendChild(div);
+    div.style.backgroundColor = `${racer.color}`;
+    div.setAttribute('id', `frog${position + 1}`);
+
+});
+
+
+
 
